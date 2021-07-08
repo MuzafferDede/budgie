f1d7e0c5f6ef004cacf4a5d9d7249458b3d5b773
Muzaffer Dede <dedemuzaffer@gmail.com>
2021-07-08 13:16:02 +0800

    new icon idea

--- client/src/components/ui/UiIcon.vue
+++ client/src/components/ui/UiIcon.vue
@@ -3,45 +3,7 @@
 </template>
 
 <script>
-import add from "../../assets/icons/svg/add.svg";
-import avatar from "../../assets/icons/svg/avatar.svg";
-import arrow from "../../assets/icons/svg/arrow.svg";
-import call from "../../assets/icons/svg/call.svg";
-import check from "../../assets/icons/svg/check.svg";
-import close from "../../assets/icons/svg/close.svg";
-import contacts from "../../assets/icons/svg/contacts.svg";
-import fullscreen from "../../assets/icons/svg/fullscreen.svg";
-import hang from "../../assets/icons/svg/hang.svg";
-import notification from "../../assets/icons/svg/notification.svg";
-import mic from "../../assets/icons/svg/mic.svg";
-import mute from "../../assets/icons/svg/mute.svg";
-import options from "../../assets/icons/svg/options.svg";
-import user from "../../assets/icons/svg/user.svg";
-import send from "../../assets/icons/svg/send.svg";
-import silence from "../../assets/icons/svg/silence.svg";
-import video from "../../assets/icons/svg/video.svg";
-import volume from "../../assets/icons/svg/volume.svg";
-
-const icons = {
-  add,
-  avatar,
-  arrow,
-  call,
-  check,
-  close,
-  contacts,
-  fullscreen,
-  hang,
-  notification,
-  mic,
-  mute,
-  options,
-  user,
-  send,
-  silence,
-  video,
-  volume,
-};
+import * as icons from "../../icons";
 
 const dimensions = {
   none: "",
